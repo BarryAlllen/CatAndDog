3ad16dd4e2f6fdaf8f2282ed825775786765d095
barry <1249683134@qq.com>
2022-06-14 18:12:11 +0800

    1.1 4层卷积层

--- catDogFight/catDog.py
+++ catDogFight/catDog.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 from tensorflow.python.keras.api.keras import layers, models
 from tensorflow.python.keras.api.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.python.keras.api.keras import optimizers
-from tensorflow.python.keras.api.keras.preprocessing import image
+
 from tensorflow.python.keras.api.keras.layers import Conv2D
 from tensorflow.python.keras.api.keras.layers import MaxPooling2D
 from tensorflow.python.keras.api.keras.layers import Flatten
 from tensorflow.python.keras.api.keras.layers import Dropout
 from tensorflow.python.keras.api.keras.layers import Dense
-from tensorflow.python.keras.api.keras.optimizers import SGD
+
 from tensorflow.python.keras.api.keras.utils import plot_model
 
-# 1.神经网络模型构建
+# 神经网络模型构建
 def cnn_model():
     # 初始化序列模型
     model = models.Sequential()
@@ -24,6 +24,11 @@ def cnn_model():
 
     #卷积层 卷积核 3x3 输出维度64
     model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+    #最大池化层
+    model.add(MaxPooling2D((2, 2)))
+
+    #卷积层 卷积核 3x3 输出维度128
+    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
     #最大池化层
     model.add(MaxPooling2D((2, 2)))
 
@@ -46,10 +51,7 @@ def cnn_model():
     # 输出各层参数
     model.summary()
 
-    # 编译模型
-    opt = SGD(lr=0.001, momentum=0.9)
-
-    # 2.配置优化器
+    # 配置优化器
     model.compile(loss='binary_crossentropy',
                   optimizer=optimizers.RMSprop(lr=1e-4),
                   metrics=['accuracy'])
@@ -61,6 +63,7 @@ def get_plot_model():
     model = cnn_model()
     plot_model(model,to_file='cnn_model.png',dpi=100,show_shapes=True,show_layer_names=True)
 
+# 训练模型
 def train_cnn_model():
     # 生成模型
     model = cnn_model()
@@ -89,7 +92,7 @@ def train_cnn_model():
         train_dir,
         # 图像统一尺寸
         target_size=(200, 200),
-        # batch数据大小
+        # batch数据大小 一次输入64张图片进行训练
         batch_size=32,
         # 返回标签数组形式 二进制
         class_mode='binary'
@@ -102,3 +105,45 @@ def train_cnn_model():
         batch_size=32,
         class_mode='binary'
     )
+
+    # 训练模型
+    history = model.fit_generator(
+        train_generator, # 定义的图片生成器
+        steps_per_epoch=100,
+        epochs=100, # 数据迭代的轮数
+        validation_data=validation_generator,
+        validation_steps=50
+    )
+
+    # 保存训练得到的的模型
+    model.save('data\catDogFight02-4.h5')
+
+    plt_result(history)
+
+# 结果可视化
+def plt_result(history):
+
+    # 准确性
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+
+    # 损失值
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(len(acc))
+    plt.plot(epochs, acc, 'r', label='Train accuracy')
+    plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    plt.title('Accuracy')
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'r', label='Train loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.title('Loss')
+    plt.legend()
+
+    plt.show()
+
+train_cnn_model()
