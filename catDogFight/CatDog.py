3bf6cd37112ea920427b4397eed8b4425e99b669
barry <1249683134@qq.com>
2022-06-16 16:16:38 +0800

    1.4 test

--- catDogFight/CatDog.py
+++ catDogFight/CatDog.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from tensorflow.python.keras.api.keras import layers, models
 from tensorflow.python.keras.api.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.python.keras.api.keras import optimizers
-
+from tensorflow.python.keras.api import keras
 from tensorflow.python.keras.api.keras.layers import Conv2D
 from tensorflow.python.keras.api.keras.layers import MaxPooling2D
 from tensorflow.python.keras.api.keras.layers import Flatten
@@ -17,50 +16,73 @@ from tensorflow.python.keras.api.keras.utils import plot_model
 # 神经网络模型构建
 from tensorflow.python.ops.confusion_matrix import confusion_matrix
 
-wh = 150
+wh = 200
+batch_size = 32
+tn = 0
 
 def cnn_model():
     # 初始化序列模型
     model = models.Sequential()
 
+    # # 官方搭建的VGG19网络
+    # conv_base = keras.applications.VGG19(weights='imagenet', include_top=False)
+    # # 设置为不可训练
+    # conv_base.trainable = False
+
     # 卷积层 卷积核 3x3 输入为200x200的RGB图片
-    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(wh, wh, 3)))
+    model.add(Conv2D(16, (3, 3), activation='relu', padding='same', input_shape=(wh, wh, 3)))
     # 最大池化层
     model.add(MaxPooling2D((2, 2)))
 
     # 卷积层 卷积核 3x3 输出维度64
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    # 最大池化层
+    model.add(MaxPooling2D((2, 2)))
+
+    # 卷积层 卷积核 3x3 输出维度128
     model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
     # 最大池化层
     model.add(MaxPooling2D((2, 2)))
 
-    # 卷积层 卷积核 3x3 输出维度128
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    # 最大池化层
-    model.add(MaxPooling2D((2, 2)))
-
-    # 卷积层 卷积核 3x3 输出维度128
-    model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
-    # 最大池化层
-    model.add(MaxPooling2D((2, 2)))
+    # # 卷积层 卷积核 3x3 输出维度128
+    # model.add(Conv2D(128, (3, 3), activation='relu', padding='same'))
+    # # 最大池化层
+    # model.add(MaxPooling2D((2, 2)))
 
     # flatten层
     model.add(Flatten())
 
+    # 全连接层
+    model.add(Dense(512, activation='relu'))
     # # 退出层
     model.add(Dropout(0.5))
 
-    # 全连接层
-    model.add(Dense(128, activation='relu'))
     # 全连接层 sigmoid
-    model.add(Dense(1, activation='sigmoid'))
+    # model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(2, activation='softmax'))
 
     # 输出各层参数
     model.summary()
 
     # 配置优化器
-    model.compile(loss='binary_crossentropy',
-                  optimizer=optimizers.RMSprop(lr=1e-4),
-                  metrics=['accuracy'])
+    # model.compile(loss='binary_crossentropy',
+    #               optimizer=optimizers.RMSprop(lr=1e-4),
+    #               metrics=['accuracy'])
+    #动态学习率为指数衰减型
+    lr_sch = keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=1e-4,
+        decay_steps=50,
+        decay_rate=0.96,
+        staircase=True
+    )
+
+    gen_optimizer = keras.optimizers.Adam(learning_rate=lr_sch) # adam优化器
+    # 编译模型
+    model.compile(
+        optimizer=gen_optimizer,
+        loss=keras.losses.BinaryCrossentropy(from_logits=True),
+        metrics=['accuracy']
+    )
 
     return model
 
@@ -101,30 +123,40 @@ def train_cnn_model():
         # 图像统一尺寸
         target_size=(wh, wh),
         # batch数据大小 一次输入64张图片进行训练
-        batch_size=32,
+        batch_size=batch_size,
         # 返回标签数组形式 二进制
-        class_mode='binary'
+        # class_mode='binary'
+        class_mode='categorical'
     )
+
+    tg = train_generator.n
 
     validation_generator = test_datagen.flow_from_directory(
         # 验证文件路径
         validation_dir,
         target_size=(wh, wh),
-        batch_size=32,
-        class_mode='binary'
+        batch_size=batch_size,
+        # class_mode='binary'
+        class_mode = 'categorical'
     )
 
     # 训练模型
     history = model.fit_generator(
         train_generator,  # 定义的图片生成器
         steps_per_epoch=100,
-        epochs=1,  # 数据迭代的轮数
+        epochs=5,  # 数据迭代的轮数
         validation_data=validation_generator,
         validation_steps=50
     )
 
+    labels = ['cats', 'dogs']
+    y_pred = model.predict(train_generator, tg // batch_size + 1)
+    y_pred_classes = np.argmax(y_pred, axis=1)
+    confusion_mtx = confusion_matrix(y_true=train_generator.classes, y_pred=y_pred_classes)
+    plot_confusion_matrix(confusion_mtx,normalize=True, target_names=labels)
+
     # 保存训练得到的的模型
-    model.save('data\catDogFight03-4.h5')
+    model.save('data\catDogFight08-3.h5')
 
     # labels = ['cat', 'dogs']
     # y_pred = model.predict(test_datagen)
@@ -133,6 +165,7 @@ def train_cnn_model():
     # plot_confusion_matrix(confusion_mtx,normalize=True,target_names=labels)
 
     plt_result(history)
+
 
 # 绘制混淆矩阵的
 def plot_confusion_matrix(cm, target_names, title='Confusion matrix', cmap=None, normalize=False):
@@ -172,6 +205,16 @@ def plot_confusion_matrix(cm, target_names, title='Confusion matrix', cmap=None,
     plt.xlabel("Predicted label\naccuracy={:0.4f}\n misclass={:0.4f}".format(accuracy, misclass))  # x方向上的标签
     plt.show()  # 显示图片
 
+# 使曲线变得顺滑
+def smooth_curve(points, factor=0.8):
+  smoothed_points = []
+  for point in points:
+    if smoothed_points:
+      previous = smoothed_points[-1]
+      smoothed_points.append(previous * factor + point * (1 - factor))
+    else:
+      smoothed_points.append(point)
+  return smoothed_points
 
 # 结果可视化
 def plt_result(history):
@@ -185,18 +228,15 @@ def plt_result(history):
 
     epochs = range(len(acc))
 
-    plt.figure(figsize=(20, 10))
-    plt.subplot(1, 2, 1)
-
-    plt.plot(epochs, acc, 'r', label='Train accuracy')
-    plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+    plt.figure()
+    plt.plot(epochs, smooth_curve(acc), 'r', label='Train accuracy')
+    plt.plot(epochs, smooth_curve(val_acc), 'b', label='Validation accuracy')
     plt.title('Accuracy')
     plt.legend()
 
-    plt.subplot(1, 2, 2)
-
-    plt.plot(epochs, loss, 'r', label='Train loss')
-    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.figure()
+    plt.plot(epochs, smooth_curve(loss), 'r', label='Train loss')
+    plt.plot(epochs, smooth_curve(val_loss), 'b', label='Validation loss')
     plt.title('Loss')
     plt.legend()
 
--- catDogFight/Test.py
+++ catDogFight/Test.py
@@ -1,3 +1,5 @@
+import os
+
 import matplotlib.pyplot as plt
 from tensorflow.python.keras.api.keras.models import load_model
 from matplotlib.pyplot import imshow
@@ -29,16 +31,71 @@ def preditCatAndDog(pil,model):
         print('cat')
         return 'cat'
 
+def preditCatAndDog2(model):
+    picPath = "H:\\Machine Learning\\CNN\\CNNProjects\\data\\test"
+    paths = []
+    names = []
+    for i in os.listdir(picPath):
+        path = os.path.join(picPath, i)
+        name = i.split('.')[0]
+        names.append(name)
+        paths.append(path)
+
+    for j in range(16):
+        print(paths[j])
+        img = Image.open(paths[j])
+        t = 200
+        w = t
+        h = t
+        # 缩放图片
+        img = img.resize((w, h))
+        # 转array格式
+        array = np.array(img) / 255.
+        # 预测
+        array = array.reshape(1, w, h, 3)
+        res = model.predict(array)
+        plt.text(0, -20, res[0][0], fontsize=30, c='b')
+        plt.axis('off')
+        plt.show()
+
+
+        print(res[0][0])
+        if res[0][0] > 0.5:
+            print('dog')
+
+        else:
+            print('cat')
+
+def preditCatAndDog3(pil,model):
+    t = 150
+    w = t
+    h = t
+    #缩放图片
+    pil = pil.resize((w,h))
+    #转array格式
+    array = np.array(pil) /255.
+    #预测
+    array = array.reshape(1,w,h,3)
+    res = model.predict(array)
+    print(res)
+    # if res[0][0]>0.5:
+    #     print('dog')
+    #     return 'dog'
+    # else:
+    #     print('cat')
+    #     return 'cat'
+
+
+
+
 #载入模型
-path='data\catDogFight02-4.h5'
+path='data\catDogFight06-3.h5'
 model = load_model(path)
 
 pil = read_image();
 imshow(np.asarray(pil))
-res = preditCatAndDog(pil,model)
-plt.text(0,-20,res,fontsize=30,c='b')
-plt.axis('off')
-plt.show()
+preditCatAndDog3(pil,model)
+
 
 
 
